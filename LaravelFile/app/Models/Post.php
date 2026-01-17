5e44bccb91c278bdce35140bbcc418a071453c09
hosomi tadashi <hosomitadasi@gmail.com>
2026-01-17 18:10:21 +0900

    LaravelFile側でデータベースとモデルを作成。

---
+++ LaravelFile/app/Models/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = ['content', 'user_id'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likes() {
+        return $this->hasMany(Like::class);
+    }
+
+    public function isLikedBy($user): bool {
+        return $this->likes()->where('user_id', $user->id)->exists();
+    }
+}
